c28177f199dd70eda4305504c145eafa4c8f3296
Christinamawel <christinamawel@gmail.com>
2022-01-21 13:35:05 -0800

    fill out README.md

--- AnimalShelter/Startup.cs
+++ AnimalShelter/Startup.cs
@@ -1,11 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using Microsoft.OpenApi.Models;
 using Microsoft.EntityFrameworkCore;
 using AnimalShelter.Models;
-using Microsoft.OpenApi.Models;
+
+
 
 namespace AnimalShelter
 {
