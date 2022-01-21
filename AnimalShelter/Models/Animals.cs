e9b9673358a62134bce7949c1678fa635a0095d0
Christinamawel <christinamawel@gmail.com>
2022-01-21 08:41:48 -0800

    create project layout and model

---
+++ AnimalShelter/Models/Animals.cs
@@ -0,0 +1,12 @@
+{
+    public class Animal
+    {
+        public int AnimalId { get; set; }
+        public string Name { get; set; }
+        public string Species { get; set; }
+        public string Breed { get; set; }
+        public int Age { get; set; }
+        public string Gender { get; set; }
+        public bool AvailableForAdoption { get; set; }
+    }
+}
