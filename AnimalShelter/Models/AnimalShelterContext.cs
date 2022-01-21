7aece3e1f537fa06a0657fe0827a0a8c872f258c
Christinamawel <christinamawel@gmail.com>
2022-01-21 10:09:47 -0800

    add validations to Animal Model

--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -15,11 +15,11 @@ namespace AnimalShelter.Models
     {
       builder.Entity<Animal>()
         .HasData(
-          new Animal { AnimalId = 1, Name = "George", Species = "Cat", Breed = "Main Coon", Age = 7, Gender = "Male", AvailableForAdoption = true },
-          new Animal { AnimalId = 2, Name = "Lilly", Species = "Cat", Breed = "Domestic short hair", Age = 12, Gender = "Female", AvailableForAdoption = true },
-          new Animal { AnimalId = 3, Name = "Rover", Species = "Dog", Breed = "Labrador Retriever", Age = 1, Gender = "Male", AvailableForAdoption = true },
-          new Animal { AnimalId = 4, Name = "Fido", Species = "Dog", Breed = "Dachshund", Age = 4, Gender = "Male", AvailableForAdoption = true },
-          new Animal { AnimalId = 5, Name = "Gerald", Species = "Hamster", Breed = "Syrian", Age = 2, Gender = "Male", AvailableForAdoption = true }
+          new Animal { AnimalId = 1, Name = "George", Species = "Cat", Breed = "Main Coon", Age = 7, Gender = "Male", SpayedOrNeutered = true ,AvailableForAdoption = true },
+          new Animal { AnimalId = 2, Name = "Lilly", Species = "Cat", Breed = "Domestic short hair", Age = 12, Gender = "Female", SpayedOrNeutered = true, AvailableForAdoption = true },
+          new Animal { AnimalId = 3, Name = "Rover", Species = "Dog", Breed = "Labrador Retriever", Age = 1, Gender = "Male", SpayedOrNeutered = true, AvailableForAdoption = true },
+          new Animal { AnimalId = 4, Name = "Fido", Species = "Dog", Breed = "Dachshund", Age = 4, Gender = "Male", SpayedOrNeutered = true, AvailableForAdoption = true },
+          new Animal { AnimalId = 5, Name = "Gerald", Species = "Hamster", Breed = "Syrian", Age = 2, Gender = "Male", SpayedOrNeutered = false, AvailableForAdoption = true }
         );
     }
   }
--- AnimalShelter/Models/Animals.cs
+++ AnimalShelter/Models/Animals.cs
@@ -1,15 +1,36 @@
+using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text.Json.Serialization
 
 namespace AnimalShelter.Models
 {
     public class Animal
     {
         public int AnimalId { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string Name { get; set; }
+
+        [Required]
+        [StringLength(20)]
         public string Species { get; set; }
+
+        [StringLength(20)]
         public string Breed { get; set; }
+
+        [Required]
+        [Range(0, 100, ErrorMessage = "Age Must be Between 0 and 100.")]
         public int Age { get; set; }
+
+        [Required]
         public string Gender { get; set; }
+
+        [Required]
+        public bool SpayedOrNeutered { get; set; }
+
+        [Required]
         public bool AvailableForAdoption { get; set; }
     }
 }
