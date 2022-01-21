e05ab32339675e1d96134152e9165bdff4638479
Christinamawel <christinamawel@gmail.com>
2022-01-21 09:50:59 -0800

    add delete in AnimalController and add seeding

---
+++ AnimalShelter/Migrations/20220121174852_seeding.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace AnimalShelter.Migrations
+{
+    public partial class seeding : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Animals",
+                columns: new[] { "AnimalId", "Age", "AvailableForAdoption", "Breed", "Gender", "Name", "Species" },
+                values: new object[,]
+                {
+                    { 1, 7, true, "Main Coon", "Male", "George", "Cat" },
+                    { 2, 12, true, "Domestic short hair", "Female", "Lilly", "Cat" },
+                    { 3, 1, true, "Labrador Retriever", "Male", "Rover", "Dog" },
+                    { 4, 4, true, "Dachshund", "Male", "Fido", "Dog" },
+                    { 5, 2, true, "Syrian", "Male", "Gerald", "Hamster" }
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 3);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 4);
+
+            migrationBuilder.DeleteData(
+                table: "Animals",
+                keyColumn: "AnimalId",
+                keyValue: 5);
+        }
+    }
+}
--- AnimalShelter/Models/AnimalShelterContext.cs
+++ AnimalShelter/Models/AnimalShelterContext.cs
@@ -2,13 +2,25 @@ using Microsoft.EntityFrameworkCore;
 
 namespace AnimalShelter.Models
 {
-    public class AnimalShelterContext : DbContext
+  public class AnimalShelterContext : DbContext
+  {
+    public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
+        : base(options)
     {
-        public AnimalShelterContext(DbContextOptions<AnimalShelterContext> options)
-            : base(options)
-        {
-        }
-
-        public DbSet<Animal> Animals { get; set; }
     }
+
+    public DbSet<Animal> Animals { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+      builder.Entity<Animal>()
+        .HasData(
+          new Animal { AnimalId = 1, Name = "George", Species = "Cat", Breed = "Main Coon", Age = 7, Gender = "Male", AvailableForAdoption = true },
+          new Animal { AnimalId = 2, Name = "Lilly", Species = "Cat", Breed = "Domestic short hair", Age = 12, Gender = "Female", AvailableForAdoption = true },
+          new Animal { AnimalId = 3, Name = "Rover", Species = "Dog", Breed = "Labrador Retriever", Age = 1, Gender = "Male", AvailableForAdoption = true },
+          new Animal { AnimalId = 4, Name = "Fido", Species = "Dog", Breed = "Dachshund", Age = 4, Gender = "Male", AvailableForAdoption = true },
+          new Animal { AnimalId = 5, Name = "Gerald", Species = "Hamster", Breed = "Syrian", Age = 2, Gender = "Male", AvailableForAdoption = true }
+        );
+    }
+  }
 }
